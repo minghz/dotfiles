ba28a02721b911865bfc21969ad7bc4f4e941700
Minghua Zhao <minghz42@gmail.com>
2022-09-29 14:38:04 -0400

    better git logs

--- functions.sh
+++ functions.sh
@@ -23,40 +23,23 @@ function git_diff_branches {
 }
 
 # Nice git logs
-function gla {
-  TOTAL_COL=$(tput cols)
-  HASH_COL_END=20
-  DATE_COL_END=32
-  NAME_COL_END=50
-  REFS_COL_SIZ=20
-  MESG_COL_END=$(($TOTAL_COL-$REFS_COL_SIZ))
-  REFS_COL_END=$TOTAL_COL
-
-  HASH="%>|($HASH_COL_END,trunc)%Cblue%h"
-  DATE="%<|($DATE_COL_END,trunc)%C(green)%cr"
-  NAME="%<|($NAME_COL_END,trunc)%C(bold blue)%an"
-  MESG="%<|($MESG_COL_END,trunc)%Creset%s"
-  REFS="%<|($REFS_COL_END,trunc)%C(yellow)%d"
-
-  git log --all --graph --pretty=tformat:"$HASH  $DATE  $NAME  $MESG  $REFS"
-}
 function gl() {
   TOTAL_COL=$(tput cols)
-  HASH_COL_END=20
-  DATE_COL_END=32
-  NAME_COL_END=50
-  REFS_COL_SIZ=20
-  MESG_COL_END=$(($TOTAL_COL-$REFS_COL_SIZ))
+  HASH_COL_END=30
+  DATE_COL_END=44
+  NAME_COL_END=60
+  MESG_COL_END=$(($NAME_COL_END + 80))
   REFS_COL_END=$TOTAL_COL
 
   HASH="%>|($HASH_COL_END,trunc)%Cblue%h"
   DATE="%<|($DATE_COL_END,trunc)%C(green)%cr"
-  NAME="%<|($NAME_COL_END,trunc)%C(bold blue)%an"
+  NAME="%<|($NAME_COL_END,trunc)%C(bold blue)%al"
   MESG="%<|($MESG_COL_END,trunc)%Creset%s"
   REFS="%<|($REFS_COL_END,trunc)%C(yellow)%d"
 
-  git log --graph --pretty=tformat:"$HASH  $DATE  $NAME  $MESG  $REFS"
+  git log --graph --pretty=tformat:"$HASH $DATE $NAME $MESG $REFS" $@ 
 }
+
 function trash(){
     mv "$@" ~/.Trash
 }
