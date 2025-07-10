9caa85ae4a561480445c5f419130960098b12f0a
Minghua Zhao <minghz42@gmail.com>
2025-07-10 11:14:50 -0400

    update git-helpers

--- git_helpers.sh
+++ git_helpers.sh
@@ -17,15 +17,15 @@ function git_prune {
 # Nice git logs
 function gl() {
   TOTAL_COL=$(tput cols)
-  HASH_COL_END=30
-  DATE_COL_END=44
-  NAME_COL_END=60
+  HASH_COL_END=14
+  DATE_COL_END=$(($HASH_COL_END + 15))
+  NAME_COL_END=$(($DATE_COL_END + 20))
   MESG_COL_END=$(($NAME_COL_END + 80))
   REFS_COL_END=$TOTAL_COL
 
   HASH="%>|($HASH_COL_END,trunc)%Cblue%h"
   DATE="%<|($DATE_COL_END,trunc)%C(green)%cr"
-  NAME="%<|($NAME_COL_END,trunc)%C(bold blue)%al"
+  NAME="%<|($NAME_COL_END,trunc)%C(bold blue)%aN"
   MESG="%<|($MESG_COL_END,trunc)%Creset%s"
   REFS="%<|($REFS_COL_END,trunc)%C(yellow)%d"
 
